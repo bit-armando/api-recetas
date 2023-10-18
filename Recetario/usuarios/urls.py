e9a190a54c75aa17d81ad57cf0b85f47299f22a8
CeciliaMendoza <al192895@alumnos.uacj.mx>
2023-10-17 23:54:35 -0600

    home

--- Recetario/usuarios/urls.py
+++ Recetario/usuarios/urls.py
@@ -3,7 +3,7 @@ from .views import home
 from .views import Login,Logout, Register
 
 urlpatterns = [
-    path('home/', home, name='home'),
+    path('', home, name='home'),
     path('logout/', Logout.as_view(), name = 'logout'),
     path('login/',Login.as_view(), name = 'login'),
     path('register/',Register.as_view(), name = 'register'),
