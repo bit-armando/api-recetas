8248e93a19c0ad33c994b709211c9f6e8c645c44
CeciliaMendoza <al192895@alumnos.uacj.mx>
2023-10-23 22:34:44 -0600

    forms

--- Recetario/recetas/forms.py
+++ Recetario/recetas/forms.py
@@ -2,6 +2,12 @@ from django import forms
 from .models import Recetas
 
 class RecetasForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
+            visible.field.widget.attrs['placeholder'] = visible.field.label
+            
     class Meta:
         model = Recetas
         exclude = ['created_date', 'author']
